31a868fe59a0db43f8f307ffa6f5f1041a40a2c7
tete389 <tete389@gmail.com>
2024-11-10 21:16:50 +0700

    up_custommer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -31,7 +31,8 @@ class CustomerController extends Controller
                     $query->where($Operator->FiltersOperators(['customers.' . $ex[0], $ex[1], $ex[2]]));
                 }
             }
-            $query->join('customer_levels', 'customer_levels.id', '=', 'customers.customer_level_id');
+
+            $query->LeftJoin('customer_levels', 'customer_levels.id', '=', 'customers.customer_level_id');
             $query->select('customers.*', 'customer_levels.name as level_name', 'customer_levels.rate as level_rate');
             if ($request->has('searchText')) {
                 $arraySearchText = ['customers.name', 'customers.phone', 'customer_levels.name'];
