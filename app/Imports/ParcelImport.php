adc8bfd570dc585d9d32d39fc2d3427d6428982f
Supachai Yuenyao <flookes2541@gmail.com>
2024-12-16 23:20:41 +0700

    update parcel import

--- app/Imports/ParcelImport.php
+++ app/Imports/ParcelImport.php
@@ -12,8 +12,9 @@ use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
 use Maatwebsite\Excel\Concerns\SkipsErrors;
 use Maatwebsite\Excel\Concerns\SkipsFailures;
 use Maatwebsite\Excel\Concerns\SkipsOnFailure;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class ParcelImport implements ToArray, WithValidation, WithStartRow, SkipsOnError, SkipsOnFailure, SkipsEmptyRows
+class ParcelImport implements ToArray, WithValidation, WithStartRow, SkipsOnError, SkipsOnFailure, SkipsEmptyRows, WithHeadingRow
 {
     use Importable, SkipsErrors, SkipsFailures;
 
@@ -29,23 +30,27 @@ class ParcelImport implements ToArray, WithValidation, WithStartRow, SkipsOnErro
         // Handle the exception how you'd like.
     }
 
+    public function headingRow(): int
+    {
+        return 1;
+    }
+
     public function array(array $rows)
     {
-
         foreach ($rows as $key => $row) {
-            $phone = $row[6];
+            $phone = $row['recipients_phone_number'];
             // if (str_starts_with($phone, '85620')) {
             //     $phone = str_replace("85620", "", $row[6]);
             // }
             $this->data[] = [
-                'zto_track_no' => $row[0],
-                'track_no' => $row[1],
-                'weight' => (float)$row[2] > (float)$row[3] ? (float)$row[2] : (float)$row[3],
-                'price' => $row[4],
-                'name' => $row[5],
+                'zto_track_no' => $row['zto_tracking_nuber'],
+                'track_no' => $row['tracking_nuber'],
+                'weight' => (float)$row['recording_weight'] > (float)$row['volume_weight'] ? (float)$row['recording_weight'] : (float)$row['volume_weight'],
+                'price' => $row['cash_on_delivery'],
+                'name' => $row['recipient'],
                 'phone' => $phone,
                 'address' => null,
-                'receipt_at' => $row[7],
+                'receipt_at' => $row['center_dispatch_time'],
                 'active' => true,
                 'status' => 'pending',
                 'created_at' => Carbon::now(),
@@ -67,14 +72,14 @@ class ParcelImport implements ToArray, WithValidation, WithStartRow, SkipsOnErro
     public function rules(): array
     {
         return [
-            '0' => 'required',
-            '1' => 'required',
-            '2' => 'required|numeric',
-            '3' => 'required|numeric',
-            '4' => 'required',
-            '5' => 'required',
-            '6' => 'required',
-            '7' => 'required|date_format:Y-m-d H:i:s',
+            'zto_tracking_nuber' => 'required',
+            'tracking_nuber' => 'required',
+            'recording_weight' => 'required|numeric',
+            'volume_weight' => 'required|numeric',
+            // 'cash_on_delivery' => 'required',
+            'recipient' => 'required',
+            'recipients_phone_number' => 'required',
+            'center_dispatch_time' => 'required|date_format:Y-m-d H:i:s',
         ];
     }
 
@@ -84,13 +89,13 @@ class ParcelImport implements ToArray, WithValidation, WithStartRow, SkipsOnErro
     public function customValidationMessages()
     {
         return [
-            '0' => 'The ZTO Track Number field is required.',
-            '2' => 'The Recording weight field is required and must be numeric.',
-            '3' => 'The Volume weight field is required and must be numeric.',
-            '4' => 'The Price field is required.',
-            '5' => 'The Recipient field is required.',
-            '6' => 'The Phone Number field is required.',
-            '7' => 'The Center dispatch time field is required and format must be Y-m-d H:i:s',
+            'zto_tracking_nuber' => 'The ZTO Track Number field is required.',
+            'recording_weight' => 'The Recording weight field is required and must be numeric.',
+            'volume_weight' => 'The Volume weight field is required and must be numeric.',
+            // '4' => 'The Price field is required.',
+            'recipient' => 'The Recipient field is required.',
+            'recipients_phone_number' => 'The Phone Number field is required.',
+            'center_dispatch_time' => 'The Center dispatch time field is required and format must be Y-m-d H:i:s',
         ];
     }
 }
